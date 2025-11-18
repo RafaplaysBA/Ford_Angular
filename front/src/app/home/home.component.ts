e4047a798e8ffa7ba4554e889d1757ab9a74ef2b
Rafael Araújo <cruzarujo486@gmail.com>
2025-11-18 11:14:40 -0300

    estilizando componentes

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
 
+ 
 }
+
+
